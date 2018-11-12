a9dd8ca9d43be0a68d9fc2c9adf7103836b8ca19
Jhainey <milevisj@gmail.com>
2018-11-12 11:39:20 -0500

    asd

--- appCiudatos/ui.R
+++ appCiudatos/ui.R
@@ -3,8 +3,8 @@ fluidPage(
   tags$head(
     tags$link(rel="stylesheet", type="text/css", href="style.css"),
     includeScript("js/iframeSizer.contentWindow.min.js"),
-    includeScript("https://public.tableau.com/javascripts/api/tableau-2.min.js")
-    includeScript("https://code.jquery.com/jquery-2.2.4.min.js")
+    includeScript("https://public.tableau.com/javascripts/api/tableau-2.min.js"),
+    includeScript("https://code.jquery.com/jquery-2.2.4.min.js"),
     includeScript("js/fuctInter.js")
   ),
   tabsetPanel(id = 'menuSup',
