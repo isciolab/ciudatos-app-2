e1e93eba41cd10c734bc5822ea63ebf55f9691a7
Jhainey <milevisj@gmail.com>
2018-11-13 17:01:30 -0500

    asdasdsad

--- appCiudatos/server.R
+++ appCiudatos/server.R
@@ -97,8 +97,8 @@ shinyServer(function(input, output, session) {
   })
   
   output$varCiudadSubj <- renderUI({
-    #id_c <- input$ciudadSubj
-    dicS <- dicCiudad2(subjDat, dicSbj)
+    id_c <- input$ciudadSubj
+    dicS <- dicCiudad(id_c, subjDat, dicSbj)
     selectorVar(dicSbj, input$last_btnCity, 'varCiudadSubjSel')
     #selectorVar(dicSbj, input$last_btnCity, 'varCiudadSubjSel')
   })
