d7ce4f26226a193e231d2a018599d13ec05d3e1e
Jhainey <milevisj@gmail.com>
2018-12-19 19:43:56 -0500

    xczxc

--- appCiudatos/server.R
+++ appCiudatos/server.R
@@ -390,7 +390,7 @@ output$baks <- renderPrint({
    # if (idG == 'linea') 
     #  g <- highchartOutput('grafCiudadObj')
     if (idG == 'barras') 
-      g <- highchartOutput('barCiudadObj')
+      g <- highchartOutput('VizObjCif')
     
     g
   })
